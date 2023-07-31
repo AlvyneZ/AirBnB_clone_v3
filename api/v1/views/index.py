0a41a45205ed0b96c634c110ac6b1dbdb80c901a
AlvyneZ <alvynew@gmail.com>
2023-07-31 20:58:04 +0300

    Added stats view

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,26 @@
 """ holds class User"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"amenities": Amenity, "cities": City, "places": Place,
+           "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    obj_counts = {}
+    for key, cls in classes.items():
+        obj_counts[key] = storage.count(cls)
+    return jsonify(obj_counts)
